5356f69960b5cd51371e81b7b04cc346126d59de
Gustavo Henrique <gugahribeiro@hotmail.com>
2020-07-24 19:45:07 -0300

    create Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
-// import { Container } from './styles';
+import Container from '../../components/Container';
+import { FormStyled, ListStyled } from './styles';
 
 import api from '../../services/api';
 import { githubRequest } from '../../services/github';
@@ -11,12 +12,37 @@ import { viacepRequest } from '../../services/viacep';
 function Home() {
 
 	const [github, SetGithub] = useState('guribeiro');
-	const [cep, SetCep] = useState('08599020');
+	const [githubResponse, SetGithubResponse] = useState({});
 
+	const [cep, SetCep] = useState('');
+	const [cepResponse, setCepResponse] = useState({});
+
+	const [time, SetTime] = useState(null);
+
+	useEffect(() => {
+		clearTimeout(time);
+		if (cep) {
+
+			const timer = setTimeout(
+				() => {
+					try {
+						loadCep(cep);
+					} catch (error) {
+						alert('error has occured!')
+					}
+				}, 1000)
+
+			SetTime(timer);
+		}
+
+		setCepResponse({});
+	}, [cep])
 
 	async function loadCep(cep) {
-		const { data } = await viacepRequest(cep);
-		console.log(data)
+		const { data, status } = await viacepRequest(cep);
+		console.log(data);
+		setCepResponse(data);
+
 	}
 
 	async function loadGitUser(github) {
@@ -24,11 +50,25 @@ function Home() {
 		console.log(data)
 	}
 	return (
-		<div>
-			<h1>Olá Mundo</h1>
-			<button onClick={loadGitUser}>github</button>
-			<button onClick={() => loadCep('08599020')}>cep</button>
-		</div>
+		<Container>
+			<h1>Studying Debounce</h1>
+			<FormStyled>
+				<label htmlFor="">CEP</label>
+				<input type="text"
+					value={cep}
+					onChange={(e) => SetCep(e.target.value)}
+				/>
+				<label htmlFor="">GITHUB</label>
+				<input
+					type="text"
+					value={github}
+					onChange={(e) => SetGithub(e.target.value)}
+				/>
+			</FormStyled>
+			<ListStyled>
+			</ListStyled>
+
+		</Container>
 	)
 }
 export default Home;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const FormStyled = styled.form`
+	margin: 30px 0;
+	display: flex;
+	flex-direction: column;
+
+	label{
+		margin-top: 15px;
+		font-size: 16px;
+	}
+
+	input{
+		flex: 1;
+		border: 1px solid #eee;
+		padding: 10px 15px;
+		border-radius: 4px;
+		font-size: 16px
+	}
+`
+
+export const ListStyled = styled.section`
+
+	padding-top: 30px;
+	margin-top: 15px;
+	border-top: 1px solid #eee;
+
+`
